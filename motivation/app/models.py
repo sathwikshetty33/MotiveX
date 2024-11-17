ba3d6db96c2f0b48cc68ba00d80d235ada7f27b9
Sathwik Shetty <sathwikshetty9876@gmail.com>
2024-11-17 08:45:00 +0530

    initial commit

--- motivation/app/models.py
+++ motivation/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class quote(models.Model):
+    quote = models.TextField()
+    author = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
--- motivation/motivation/settings.py
+++ motivation/motivation/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
